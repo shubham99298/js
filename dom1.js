a9bf6fc39ca4b323572cde8887d8f1b1ce2431c5
Shubham <shubhamlata2002@gmail.com>
2023-08-10 11:15:02 +0530

    js

---
+++ dom1.js
@@ -0,0 +1,52 @@
+// document.getElementsByClassName('values')[0].onclick=function(){
+     
+//     let a=document.getElementsByClassName('1')[0].value;
+//     let b=document.getElementsByClassName('2')[0].value;
+
+//     let val=document.getElementsByClassName('values')[0].value;
+
+//     if(val==='+'){
+//         console.log(parseInt(a)+parseInt(b));
+//     }
+
+//     if(val==='-'){
+//         console.log(a-b)
+//     }
+
+//     if(val==='*'){
+//         console.log(a*b)
+//     }
+
+//     if(val==='/'){
+//         console.log(a/b)
+//     }
+
+//    }
+//    document.getElementsByClassName("btn")[0].onclick=function(){
+ 
+
+//     let a=document.getElementsByClassName('inp');
+
+//     for(let i=0; i<a.length;i++){
+//         console.log(a[i].value);
+//     }
+//     for(let i of a){
+//         console.log(i.value);
+//     }
+//     for(let i in a){
+//         console.log(a[i].value);
+    
+// }
+//    }
+
+//    document.querySelector('.btn').onclick=function(){
+//     let a=document.querySelectorAll('.inp');
+
+//     console.log(a )
+//    }
+
+
+document.getElementById('container').onmouseover=function(){
+    
+
+}
